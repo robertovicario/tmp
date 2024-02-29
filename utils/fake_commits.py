9c1ea0c98e13e099c7fbaead6c82a15c3d65efe5
Roberto Vicario <rvicariodev@gmail.com>
2024-03-01 00:00:00 +0100

    updates

--- utils/fake_commits.py
+++ utils/fake_commits.py
@@ -1,11 +1,12 @@
 import os
 
-def fake_commits(n):
+def fake_commits(n, year, month, day):
+    date = f"{year}-{month}-{day}T00:00:00"
     for i in range(n):
         with open('tmp.txt', 'a') as tmp:
-            tmp.write("#\n")
+            tmp.write("#" + str(i) + "\n")
 
         os.system('git add .')
-        os.system('git commit -m updates')
+        os.system(f'git commit -m updates --date="{date}"')
 
-fake_commits(20)
+fake_commits(20, "2024", "03", "01")

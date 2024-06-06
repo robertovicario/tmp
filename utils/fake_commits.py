2016eedc458a3e57438183e2b8981e40d3aac4e5
Roberto Vicario <rvicariodev@gmail.com>
2024-06-06 14:20:00 +0200

    update

--- utils/fake_commits.py
+++ utils/fake_commits.py
@@ -2,9 +2,18 @@ import os
 
 from torch import rand
 
-for i in range(1):
+def operation():
     with open('tmp.txt', 'a') as file:
-        file.write('#')
+        file.write('#\n')
 
-os.system("git add .")
-os.system(f'git commit --date="2024-5-{str(rand)} -m updates')
+    os.system("git add .")
+    os.system(f'git commit --date="2024-5-{str(rand)}" -m updates')
+
+for i in range(10):
+    operation()
+
+for i in range(20):
+    operation()
+
+for i in range(30):
+    operation()
